b7c8e8b5efa8eab095d08ba26b9da55fed7bdab5
ccanizam <carlos.canizares@isobar.com>
2023-03-14 15:47:05 +0100

    N26 - Feature 1: Agent important information component: first version of the case important information component ready

--- force-app/main/default/lwc/caseImportantInformation/caseImportantInformation.js
+++ force-app/main/default/lwc/caseImportantInformation/caseImportantInformation.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api} from 'lwc';
+import getContactProductImportantData from '@salesforce/apex/CaseImportantInformationController.getContactProductImportantData';
 
-export default class CaseImportantInformation extends LightningElement {}
+const columns = [
+    { label: 'Contact Product', fieldName: 'contactProduct'},
+    { label: 'Contact Home Country', fieldName: 'contactHomeCountry'}
+];
+
+const data = [
+    {
+        id: 'a',
+        contactProduct: 'Test Product',
+        contactHomeCountry: 'Home Country'
+    }
+];
+export default class CaseImportantInformation extends LightningElement {
+    data = data;
+    columns = columns;
+    @api
+    recordId;
+    @track
+    contactProduct = null;
+    @track
+    contactHomeCountry = null;
+
+    renderedCallback(){
+        this.retreiveData();
+    }
+
+    retreiveData(){
+        let globalContext = this;
+        getContactProductImportantData({caseId: this.recordId}).then(productWrapper => {
+            globalContext.contactProduct = productWrapper.contactProduct;
+            globalContext.contactHomeCountry = productWrapper.contactHomeCountry;
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+}
